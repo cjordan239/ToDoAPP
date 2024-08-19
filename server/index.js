01558a196ddd1c43ccf53cc23ba7a20c1fa6e763
cjordan239 <chris.jordanig@gmail.com>
2024-08-19 11:42:12 +0700

    add cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const port = 3001;
 
 const userRoutes = require('./src/Routes/userRoute')
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/users', userRoutes);
 
