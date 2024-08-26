b69a60b4b9d66d400ee9ca079442f9cf1383dc9d
cjordan239 <chris.jordanig@gmail.com>
2024-08-26 22:23:54 +0700

    feat/adding navbar

---
+++ todoapp/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import NavigationBar from '../components/Molecule/NavigationBar'
+const DasboardPage = () => {
+  return (
+    <div><NavigationBar/></div>
+  )
+}
+
+export default DasboardPage
